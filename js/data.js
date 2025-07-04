b6163177905f8ee923a2635e0b4fd2db8c36d708
st0971 <117559430+st0971@users.noreply.github.com>
2025-07-04 14:48:58 +0800

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,17 @@
+const products = {
+  1: {
+    id: 1,
+    name: "商品名稱1",
+    price: 399,
+    image: "images/samplephoto.png",
+    description: "這是商品1的詳細描述。"
+  },
+  2: {
+    id: 2,
+    name: "商品名稱2",
+    price: 499,
+    image: "images/samplephoto.png",
+    description: "這是商品2的詳細描述。"
+  }
+  // ... 其他商品
+};
